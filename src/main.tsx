ce9297817c628ce155db276850122042ba419aec
yuvraj gupta <yuvrajam12@gmail.com>
2024-03-14 20:54:35 +0530

    first

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import AuthProvider from './context/AuthContext';
+import { QueryProvider } from './lib/react-query/QueryProvider';
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+    <AuthProvider>
+    <QueryProvider>
+    <App/>
+
+    </QueryProvider>
+    </AuthProvider>
+    </BrowserRouter>
+)
